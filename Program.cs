54cd7306d38a7f9e9918609d94a48c0aaf8de370
Ivan Semenikhin <iv.semenikhin@gmail.com>
2023-10-23 20:26:17 +0300

    Создал проект. Написал метды для печати и заполнения массива

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина
+// которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, либо задать
+// на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, лучше обойтись
+// исключительно массивами.
+
+// Примеры:
+// [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+// [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+// [“Russia”, “Denmark”, “Kazan”] → []
+
+void PrintArray(string[] array)
+{
+    System.Console.WriteLine();
+    foreach (string item in array)
+    {
+        System.Console.Write(item + " ");
+    }
+}
+
+void FillArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Console.ReadLine()!;
+    }
+}
+
+System.Console.WriteLine("Введите данные: ");
+string[] array = new string[10];
+FillArray(array);
+PrintArray(array);
